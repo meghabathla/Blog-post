224b23e9534591f39de8ed1f0295132721d6e5d4
Megha-Bathla <bathlamegha6@gmail.com>
2024-03-12 00:32:24 +0530

    Add more code in index and Header file

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { UseSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
   return <div>Header</div>;
 }
 

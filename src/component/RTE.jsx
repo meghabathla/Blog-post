d3aa896a1c741963341f2dbcec8798faba581006
Megha-Bathla <bathlamegha6@gmail.com>
2024-03-17 00:36:51 +0530

    code PostForm, RTE and update index file in component

---
+++ src/component/RTE.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+//real Time editor creating this component without forwardingRef hook. here we are learning how to stay updated state of this hook through react hook form
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb- pl-1"> {label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+}

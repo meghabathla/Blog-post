a74d7c474faa63b669dfd11d929fa58bcedcfb1c
Megha-Bathla <bathlamegha6@gmail.com>
2024-04-26 19:44:46 +0530

    mande changes in UI

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({
   children,
   type = "button",
-  bgColor = "bg-blue-600",
+  bgColor = "bg-blue-500",
   textColor = "text-white",
   className = "",
   ...props

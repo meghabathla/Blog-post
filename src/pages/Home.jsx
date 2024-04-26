2f565c3d8c01ebb3b36368fd3259b6030ca6a116
Megha-Bathla <bathlamegha6@gmail.com>
2024-04-26 23:56:41 +0530

    refactor code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,30 @@
-import React, { useEffect, useState } from "react";
-import appwriteService from "../appwrite/config";
-import { Container, PostCard } from "../component";
+import React from "react";
+
+import { Container } from "../component";
+import { useSelector } from "react-redux";
 
 function Home() {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    appwriteService.getPosts().then((posts) => {
-      if (posts) {
-        setPosts(posts.documents);
-      }
-    });
-  }, []);
+  const isUserLoggedIn = useSelector((state) => state.auth.status);
 
-  if (posts.length === 0) {
-    return (
-      <div className="w-full py-8 mt-4 text-center">
-        <Container>
-          <div className="flex flex-wrap w-full min-h-96">
-            <div className="p-2 w-full">
+  return (
+    <div className="w-full py-8 mt-4 text-center">
+      <Container>
+        <div className="flex flex-wrap w-full min-h-96">
+          <div className="p-2 w-full">
+            {!isUserLoggedIn ? (
               <h1 className=" p-2  text-2xl font-bold hover:text-gray-500">
                 Login to read posts
               </h1>
-              <p className="p-4">
-                This blog site is where you can write or read blogs and
-                articles. For new bloggers seeking blog app free of charge to
-                kickstart their writing journey. Publish your passions your way.
-                Whether you'd like to share your knowledge, experiences or the
-                latest news, create a unique and beautiful blog.
-              </p>
-            </div>
+            ) : null}
+
+            <p className="p-4">
+              This blog site is where you can write or read blogs and articles.
+              For new bloggers seeking blog app free of charge to kickstart
+              their writing journey. Publish your passions your way. Whether
+              you'd like to share your knowledge, experiences or the latest
+              news, create a unique and beautiful blog.
+            </p>
           </div>
-        </Container>
-      </div>
-    );
-  }
-  return (
-    <div className="w-full py-8">
-      <Container>
-        <div className="flex flex-wrap">
-          {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
-              <PostCard {...post} />
-            </div>
-          ))}
         </div>
       </Container>
     </div>
